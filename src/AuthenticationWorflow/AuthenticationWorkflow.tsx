9af448752549c51a6014900fe9f97b278697465e
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-07-21 14:04:42 +0500

    updates

--- src/AuthenticationWorflow/AuthenticationWorkflow.tsx
+++ src/AuthenticationWorflow/AuthenticationWorkflow.tsx
@@ -3,21 +3,33 @@ import GetStarted from "../get-started-sdk/get-started-sdk";
 import axios from "axios";
 import config from '../config.json';
 
-async function FetchToken() {
-  if(config.serverUrl)
-  try {
-    const response = await axios.get(`${config.serverUrl}/rtc/${config.channelName}/1/uid/${config.uid}/?expiry=${config.tokenExpiryTime}`);
-    console.log(response.data.rtcToken);
-    config.rtcToken = response.data.rtcToken; // Update the token in the config file
-  } catch (error) {
-    console.error(error);
-    throw error;
+async function fetchRTCToken(channelName: string) {
+  if (config.serverUrl !== "") {
+    try {
+      const response = await axios.get(
+        `${config.proxyUrl}${config.serverUrl}/rtc/${channelName}/1/uid/${config.uid}/?expiry=${config.tokenExpiryTime}`,
+        {
+          headers: {
+            "X-Requested-With": "XMLHttpRequest",
+          },
+        }
+      );
+      console.log("RTC token fetched from server: ", response.data.rtcToken);
+      return response.data.rtcToken;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  } else {
+    return config.rtcToken;
   }
 }
 
 function AuthenticationWorkflow() {
   useEffect(() => {
-    FetchToken().catch((error) => {
+    fetchRTCToken("hussain").then((token) => {
+      config.rtcToken = token;
+    }).catch((error) => {
       console.error(error);
     });
   }, []);
