fd7505bf15a9df0cba4293c7fcc00b784cb33108
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-08-02 23:48:43 +0500

    empty variables

--- src/agora-manager/config.tsx
+++ src/agora-manager/config.tsx
@@ -2,10 +2,10 @@ import { EncryptionMode, UID } from "agora-rtc-sdk-ng";
 
 const config: configType = {
   uid: 0,
-  appId: "9d2498880e934632b38b0a68fa2f1622",
+  appId: "",
   channelName: "test",
   rtcToken: "",
-  serverUrl: "https://agora-token-service-production-b75a.up.railway.app/",
+  serverUrl: "",
   proxyUrl: "http://localhost:8080/",
   tokenExpiryTime: 600,
   token: "",
