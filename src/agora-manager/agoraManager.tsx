02360ff2a0bb3e055090a9c6207bf4d76dd9665e
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-11-14 18:44:04 +0500

    comments

--- src/agora-manager/agoraManager.tsx
+++ src/agora-manager/agoraManager.tsx
@@ -1,4 +1,4 @@
-// AgoraManager.tsx
+// Import necessary components and hooks from Agora SDK and React
 import {
   LocalVideoTrack,
   RemoteUser,
@@ -11,32 +11,42 @@ import {
 import React, { createContext, useContext } from "react";
 import { IMicrophoneAudioTrack, ICameraVideoTrack } from "agora-rtc-sdk-ng";
 import { configType } from "./config";
+
+// Define the shape of the Agora context
 interface AgoraContextType {
   localCameraTrack: ICameraVideoTrack | null;
   localMicrophoneTrack: IMicrophoneAudioTrack | null;
   children: React.ReactNode;
 }
 
+// Create the Agora context
 const AgoraContext = createContext<AgoraContextType | null>(null);
 
-export const AgoraProvider: React.FC<AgoraContextType> = ({ children, localCameraTrack , localMicrophoneTrack }) => (
+// AgoraProvider component to provide the Agora context to its children
+export const AgoraProvider: React.FC<AgoraContextType> = ({ children, localCameraTrack, localMicrophoneTrack }) => (
   <AgoraContext.Provider value={{ localCameraTrack, localMicrophoneTrack, children }}>
     {children}
   </AgoraContext.Provider>
 );
 
+// Custom hook to access the Agora context
 export const useAgoraContext = () => {
   const context = useContext(AgoraContext);
   if (!context) throw new Error("useAgoraContext must be used within an AgoraProvider");
   return context;
 };
 
+// AgoraManager component responsible for handling Agora-related logic and rendering UI
 export const AgoraManager = ({ config, children }: { config: configType; children: React.ReactNode }) => {
+  // Retrieve local camera and microphone tracks and remote users
   const { isLoading: isLoadingCam, localCameraTrack } = useLocalCameraTrack();
   const { isLoading: isLoadingMic, localMicrophoneTrack } = useLocalMicrophoneTrack();
   const remoteUsers = useRemoteUsers();
+
+  // Publish local tracks
   usePublish([localMicrophoneTrack, localCameraTrack]);
 
+  // Join the Agora channel with the specified configuration
   useJoin({
     appid: config.appId,
     channel: config.channelName,
@@ -44,16 +54,20 @@ export const AgoraManager = ({ config, children }: { config: configType; childre
     uid: config.uid,
   });
 
+  // Check if devices are still loading
   const deviceLoading = isLoadingMic || isLoadingCam;
   if (deviceLoading) return <div>Loading devices...</div>;
 
+  // Render the AgoraProvider and associated UI components
   return (
     <AgoraProvider localCameraTrack={localCameraTrack} localMicrophoneTrack={localMicrophoneTrack}>
       {children}
       <div id="videos">
+        {/* Render the local video track */}
         <div className="vid" style={{ height: 300, width: 600 }}>
           <LocalVideoTrack track={localCameraTrack} play={true} />
         </div>
+        {/* Render remote users' video and audio tracks */}
         {remoteUsers.map((remoteUser) => (
           <div className="vid" style={{ height: 300, width: 600 }} key={remoteUser.uid}>
             <RemoteUser user={remoteUser} playVideo={true} playAudio={true} />
@@ -64,4 +78,5 @@ export const AgoraManager = ({ config, children }: { config: configType; childre
   );
 };
 
+// Export the AgoraManager component as the default export
 export default AgoraManager;
