498185e2f11c4bf08fe87960b5811f98ad90d15c
Hussain Khalid <72780625+hussain-khalid@users.noreply.github.com>
2023-11-27 11:32:53 +0500

    updates

--- src/spatial-audio/spatialAudio.tsx
+++ src/spatial-audio/spatialAudio.tsx
@@ -12,6 +12,10 @@ import {
 } from "agora-rtc-react";
 import AuthenticationWorkflowManager from "../authentication-workflow/authenticationWorkflowManager";
 
+const extension = new SpatialAudioExtension({
+  assetsPath: "./node_modules/agora-extension-spatial-audio/external/",
+});
+
 function SpatialAudio() {
   const agoraEngine = useRTCClient(AgoraRTC.createClient({ codec: "vp8", mode: "rtc" }));
 
@@ -66,32 +70,51 @@ function SpatialAudioComponent() {
 
 function AgoraExtensionComponent() {
   const [isMediaPlaying, setMediaPlaying] = useState(false);
+  const [isRegistered, setRegistered] = useState(false);
+
   const [audioFileTrack, setAudioFileTrack] = useState<
     IBufferSourceAudioTrack | null
   >(null);
   const remoteUsers = useRemoteUsers();
   const numberOfRemoteUsers = remoteUsers.length;
   const remoteUser = remoteUsers[numberOfRemoteUsers - 1];
-  const extension = useRef<SpatialAudioExtension | null>(null);
-  const processors = useRef<Map<string|UID, SpatialAudioProcessor>>(
+  const processors = useRef<Map<string | UID, SpatialAudioProcessor>>(
     new Map()
   );
-  const [distance, setDistance] = useState(0); 
+  const [distance, setDistance] = useState(0);
   const mediaPlayerKey = "media-player";
 
   useEffect(() => {
-    const initializeSpatialProcessor = async () => {
-      if (extension.current === null) {
-        extension.current = new SpatialAudioExtension({
-          assetsPath: "./node_modules/agora-extension-spatial-audio/external/",
-        });
-        AgoraRTC.registerExtensions([extension.current]);
-      }
 
-      if (remoteUser) {
+    const cleanupFunction = () => {
+      console.log("hook destroyed");
+    
+      try {
+        const disablePromises = Array.from(processors.current.values()).map(async (processor) => {
+          if (processor) {
+            await processor.disable();
+          }
+        });
+    
+        Promise.all(disablePromises).catch((reason) => console.log(reason));
+        processors.current.clear();
+        AgoraRTC.registerExtensions([]);
+      } catch (error) {
+        console.error("Error in cleanup:", error);
+      }
+    };
+    
+    const initializeSpatialProcessor = async () => {
+      if(!isRegistered)
+      {
+        console.log("Registering spatial audio extension...");
+        AgoraRTC.registerExtensions([extension]);
+        setRegistered(true);
+      }
+      if (remoteUser && !processors.current.has(remoteUser.uid)) {
         console.log("Initializing spatial audio processor...");
         try {
-          const processor = extension.current.createProcessor();
+          const processor = extension.createProcessor();
           processors.current.set(remoteUser.uid, processor);
           remoteUser.audioTrack?.pipe(processor).pipe(remoteUser.audioTrack.processorDestination);
           await processor.enable();
@@ -100,36 +123,17 @@ function AgoraExtensionComponent() {
         }
       }
     };
+  
 
     void initializeSpatialProcessor();
 
-    return () => {
-      const disableSpatialAudio = async () => {
-        console.log("hook destroyed");
-        if(remoteUser)
-        {
-            const processor = processors.current.get(remoteUser.uid);
-            processor?.unpipe();
-            remoteUser.audioTrack?.unpipe();
-            await processor?.disable();
-        }
-        if(audioFileTrack)
-        {
-          const processorMediaPlayer = processors.current.get(mediaPlayerKey);
-          processorMediaPlayer?.unpipe();
-          audioFileTrack?.unpipe();
-          await processorMediaPlayer?.disable();
-        }
-      };
-      void disableSpatialAudio();
-    };
-  }, []);
+    return cleanupFunction;
+  }, [remoteUser, isRegistered]);
 
   const updatePosition = () => {
-    if(isMediaPlaying === false && !remoteUser)
-    {
-        console.log("Currently, there is no remote user in the channel. To test spatial audio, please click the 'Play Audio File' button");
-        return;
+    if (isMediaPlaying === false && !remoteUser) {
+      console.log("Currently, there is no remote user in the channel. To test spatial audio, please click the 'Play Audio File' button");
+      return;
     }
     if (isMediaPlaying) {
       const processorRef = processors.current.get(mediaPlayerKey);
@@ -158,24 +162,22 @@ function AgoraExtensionComponent() {
     updatePosition();
   }, [distance]);
 
-  const PlayMediaFile = () =>
-  {
+  const PlayMediaFile = () => {
     const processor = processors.current.get(mediaPlayerKey);
-    if(!processor)
-    {
-      const processorRef = extension.current!.createProcessor();
+    if (!processor) {
+      const processorRef = extension.createProcessor();
       processors.current.set(mediaPlayerKey, processorRef);
       AgoraRTC.createBufferSourceAudioTrack({
         source: "../src/assets/sample.wav", // Replace with the actual audio file path
-      }).then((track) => 
-      {
+      }).then((track) => {
         track.pipe(processorRef).pipe(track.processorDestination);
         setAudioFileTrack(track);
       })
-      .catch((error) => console.log(error));
+        .catch((error) => console.log(error));
     }
     setMediaPlaying(!isMediaPlaying);
   };
+
   return (
     <div>
       <button onClick={increaseDistance}>+</button>
